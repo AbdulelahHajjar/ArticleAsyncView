d75b5d4019ff56bb6619535907682eef70dc343a
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2024-08-21 00:26:04 +0300

    chore(common): Update License Usage

--- ArticleAsyncView/Modules/AsyncView/Views/AsyncView.swift
+++ ArticleAsyncView/Modules/AsyncView/Views/AsyncView.swift
@@ -5,6 +5,30 @@
 //  Created by Abdulelah Hajjar on 31/05/2024.
 //
 
+/*
+MIT License
+
+Copyright (c) 2024 Abdulelah Hajjar
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 import SwiftUI
 
 struct AsyncView<Response, Content: View>: View {
@@ -27,10 +51,10 @@ extension AsyncView {
     @ViewBuilder var responseView: some View {
         switch response {
             case .loaded, .loading, .idle:
-                let isLoading = response.isLoading
+                let isLoaded = response.loadedValue != nil
                 content(response.loadedValue ?? mockResponse)
-                    .redacted(reason: isLoading ? .placeholder : [])
-                    .allowsHitTesting(isLoading ? false : true)
+                    .redacted(reason: isLoaded ? [] : .placeholder)
+                    .allowsHitTesting(isLoaded)
             case .failed(let error):
                 failureView(error: error)
         }
@@ -83,7 +107,9 @@ extension AsyncView {
     }
     
     private func loadResponseIfPossible() async {
-        guard !response.isLoading else { return }
+        if case .loading = response {
+            return
+        }
         do {
             try Task.checkCancellation()
             withAnimation { self.response = .loading }
